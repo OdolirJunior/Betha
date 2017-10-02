e5f40b72d3f03282d3a45da69e2c08d5fd671da6
Odolir Daniel dos Santos Junior <odolir@edu.univali.br>
2017-10-02 12:48:03 -0300

    removendo

--- Project_Betha/src/main/java/br/com/odolirjunior/domain/enumeration/Status.java
+++
@@ -1,8 +0,0 @@
-package br.com.odolirjunior.domain.enumeration;
-
-/**
- * The Status enumeration.
- */
-public enum Status {
-    AGUARDANDO_ATENDIMENTO, EM_ATENDIMENTO, FINALIZADO
-}
--- Project_Betha/src/main/webapp/app/components/form/pagination.constants.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('projectBethaApp')
-        .constant('paginationConstants', {
-            'itemsPerPage': 20
-        });
-})();
